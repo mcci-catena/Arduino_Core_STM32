978ad090f40a7eba758532ff82555b5c2d72e90b
Terry Moore <tmm@mcci.com>
2021-05-02 19:39:15 -0400

    Make trusted boot the default: major/breaking change, so now V3.0.0-1

--- cores/arduino/Arduino.h
+++ cores/arduino/Arduino.h
@@ -52,7 +52,7 @@
 ///     \ref _mcci_arduino_version_calc() to compare relative versions.
 ///
 #define	_mcci_arduino_version	\
-  _mcci_arduino_version_calc(2, 9, 0, 2)	/* v2.9.0-2 */
+  _mcci_arduino_version_calc(3, 0, 0, 1)	/* v3.0.0-2 */
 
 ///
 /// \brief get major version code from semantic version value
