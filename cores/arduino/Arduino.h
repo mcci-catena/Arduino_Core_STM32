4a864824d1e04a0454d0a5e263b60c91b5870bbc
Terry Moore <tmm@mcci.com>
2021-05-21 12:45:07 -0400

    This is v3.0.1-2

--- cores/arduino/Arduino.h
+++ cores/arduino/Arduino.h
@@ -52,7 +52,7 @@
 ///     \ref _mcci_arduino_version_calc() to compare relative versions.
 ///
 #define	_mcci_arduino_version	\
-  _mcci_arduino_version_calc(3, 0, 1, 1)	/* v3.0.1-1 */
+  _mcci_arduino_version_calc(3, 0, 1, 2)	/* v3.0.1-2 */
 
 ///
 /// \brief get major version code from semantic version value
